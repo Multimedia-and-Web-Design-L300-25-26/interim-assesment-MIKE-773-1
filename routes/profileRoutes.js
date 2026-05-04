8470632e1b71df944fe687bcc59e43a6ea919610
MIKE-773 <mcthomas@st.ug.edu.gh>
2026-05-04 16:45:29 +0000

    Replace repository contents with new workspace files

--- routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
-const authController = require('../controllers/authController');
-
-router.get('/', authMiddleware, authController.getProfile);
-
-module.exports = router;
